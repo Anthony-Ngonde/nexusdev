ca14fdac32c46338c6297d4570a68ee4fc4d7631
Anthony Ngonde <anto.ngonde@gmail.com>
2024-12-09 13:56:10 +0300

    added navigation to routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from "./routes/HomePage/HomePage"
+import DashboardPage from './routes/DashboardPage/DashboardPage';
+import ChatPage from './routes/ChatPage/ChatPage'
+// import RootLayout from './layouts/RootLayout/RootLayout';
+// import DashboardLayout from './layouts/DashboardLayout/DashboardLayout';
+// import SignInPage from './routes/SignInPage/SignInPage';
+// import SignUpPage from './routes/SignUpPage/SignUpPage';
+
+
+
+
+const router = createBrowserRouter([
+//   {
+//     element: <RootLayout/>,
+//     children:[
+//       {
+//         path:"/sign-in/*",
+//         element: <SignInPage />,
+//       },
+//       {
+//         path:"/sign-up/*",
+//         element: <SignUpPage />,
+//       },
+      {
+        path:"/",
+        element: <HomePage />,
+      },
+//       {
+//         element: <DashboardLayout />,
+//         children:[
+          {
+            path:"/dashboard",
+            element:<DashboardPage />
+          },
+          {
+            path:"/dashboard/chats/:id",
+            element:<ChatPage />,
+          }
+//         ]
+//       }
+//     ]
+//   },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
